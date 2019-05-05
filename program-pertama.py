8ee0ba7f5ebf7b409ea189de66e76cce0355dd94
Maskumambang Padmanegara <43704863+Dimaspadma@users.noreply.github.com>
2019-05-05 09:43:21 +0700

    menambah program kelulusan

--- program-pertama.py
+++ program-pertama.py
@@ -1,2 +1,20 @@
 # Mencetak kata Hello World
 print('Hello World')
+
+# Membuat program kelulusan
+# Mendefinisikan nilai
+nilai = 80
+
+# Jika nilai lebih besar sama dengan 76 dan kurang dari sama dengan 100, maka lulus
+#if nilai >= 76 and nilai <= 100:
+if 76 <= nilai <= 100
+  print('Selamat anda lulus')
+  
+# Jika nilai di bawah 76 dan lebih dari sama dengan 0, maka tidak lulus
+# elif nilai < 76 and nilai >= 0:
+elif 0 <= nilai < 76:
+  print('Selamat anda tidak lulus')
+
+# Jika tidak memenuhi semuanya
+else:
+  print('Masukan nilai dengan benar!')
